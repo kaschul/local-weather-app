0da6f6deb14a9c97e542fa7471f61268b19d001f
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 18:20:45 -0800

    update files

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getCurrentWeather(city: string, country: string){
+    this.httpClient.get(`api.openweathermap.org/data/2.5/weather?q=${name}&appid=${environment.appId}`)
+  }
 }
+
+
