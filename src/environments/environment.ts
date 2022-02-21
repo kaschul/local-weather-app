4f1d731f54f40e082cf607b432fad6188f13e4d3
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 18:20:08 -0800

    update files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: 'f5d02258b65ce08d1338caeff912a8b0'
 };
 
 /*
