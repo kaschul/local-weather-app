ac03572166c0daf03bdaa49d9c1e5d28b3f1ac0d
kaschul <47723396+kaschul@users.noreply.github.com>
2022-02-20 20:26:36 -0800

    added files

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ICurrentWeatherData } from './icurrent-weather-data';
+import { symlinkSync } from 'fs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,18 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) { }
 
   getCurrentWeather(city: string, country: string){
-    this.httpClient.get(`api.openweathermap.org/data/2.5/weather?q=${name}&appid=${environment.appId}`)
+    return this.httpClient.get<ICurrentWeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${environment.appId}`)
+  }
+
+  private transformToICurrentWeather(data: ICurrentWeatherData){
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: new Date(data.dt * 1000),
+      temperature: data.main.temp * 9/5 - 459.67,
+      description: data.weather[0].description,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    }
   }
 }
 
